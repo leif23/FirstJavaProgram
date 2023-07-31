72064a65633ce7bef211e46929f53381d134a2dc
leif23 <112565867+leif23@users.noreply.github.com>
2023-07-31 09:39:11 -0400

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package Fundamentals.FirstJavaProgram;
+
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        // \n means net line or println() todo the next line
+        System.out.print("My name is Leif \n");
+        System.out.print("I am 24 years old \n");
+        System.out.print("My home town is New York ");
+    }
+}
